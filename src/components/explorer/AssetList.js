987b9ba96a8ba8aa1ceff1b02eeda38df99aec25
devbluesky111 <talent.dev111@gmail.com>
2021-09-13 13:16:12 +0300

    eliminate some grid style for weird incomplete last rows

--- src/components/explorer/AssetList.js
+++ src/components/explorer/AssetList.js
@@ -64,7 +64,7 @@ function AssetList(props) {
                 <div    
                     className={cn(
                         "relative w-full mb-24",
-                        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4"
+                        "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
                     )}
                 >
                     {
--- src/components/market/index.js
+++ src/components/market/index.js
@@ -95,7 +95,7 @@ const Market = (props) => {
                     { isLoading ? <LoadingIndicator /> : 
                         <div className={cn(
                             "relative w-full mb-24",
-                            "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4"
+                            "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4"
                         )}>
                             {listings && listings['success'] ? listings['data'].map((listing, index) =>
                                 <AssetCard
