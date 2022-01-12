9a49373f5c7db116bbf9cb65db6dc7b4f2163dc0
Tobias Kopelke <kopelke@gmail.com>
2022-01-12 20:37:20 +0100

    setup tests for filters

--- src/api/filter.spec.js
+++ src/api/filter.spec.js
@@ -1,7 +1,27 @@
 import { filter } from './filter'
 
 describe('filter', () => {
-    it.each([['', {}]])('should generate "%s" correctly', (result, params) => {
+    it.each([
+        ['', {}],
+        ['&collection_whitelist=a,b', { collections: ['a', 'b'] }],
+        ['&page=a', { page: 'a' }],
+        ['&min_assets=2', { bundles: true }],
+        ['&owner=a', { user: 'a' }],
+        ['&schema_name=a', { schema: 'a' }],
+        ['&match=a', { name: 'a' }],
+        ['&limit=a', { limit: 'a' }],
+        ['&order=a', { orderDir: 'a' }],
+        ['&sort=a', { sortBy: 'a' }],
+        ['&asset_id=a', { asset_id: 'a' }],
+        ['&template_data.rarity=a', { rarity: 'a' }],
+        ['&template_data.variant=a', { variant: 'a' }],
+        ['&seller=a', { seller: 'a' }],
+        ['&ids=a,b', { ids: ['a', 'b'] }],
+        ['&bidder=a', { bidder: 'a' }],
+        ['&participant=a', { winner: 'a' }],
+        ['&template_whitelist=a,b', { template_ids: ['a', 'b'] }],
+        ['&template_id=a', { template_id: 'a' }],
+    ])('should generate "%s" correctly', (result, params) => {
         expect(filter(params)).toEqual(result)
     })
 })
