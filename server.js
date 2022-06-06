d8de12236d681652a54aa8fe1958c21326e64045
James <james.onsite@gmail.com>
2022-06-05 21:02:35 -0500

    port change

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.prepare()
             return handle(req, res, parsedUrl)
         })
 
-        server.listen(3001, (err) => {
+        server.listen(3000, (err) => {
             if (err) throw err
-            console.log('> Ready on http://localhost:3001')
+            console.log('> Ready on http://localhost:3000')
         })
     })
     .catch((ex) => {
